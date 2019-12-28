dbddb57235a25229eff18359b5efb2043e27dc0e
bigeye2599 <bigeye2599@gmail.com>
2019-12-28 21:14:23 +0900

    15. 가게 추가 - 2

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -14,6 +14,11 @@ public class Restaurant {
 
     public Restaurant() {}
 
+    public Restaurant(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
     public Restaurant(Long id, String name, String address) {
         this.id = id;
         this.name = name;
@@ -22,6 +27,10 @@ public class Restaurant {
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
